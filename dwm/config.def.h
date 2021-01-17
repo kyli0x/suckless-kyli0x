b50f09ce6849a0f1cb5c19e55b5278de71cf4c1e
kyli0x <kyli0x@protonmail.ch>
2021-01-16 19:39:40 -0500

    changed quit/restart dwm shortcut

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -76,7 +76,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,                       XK_q,      killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -100,7 +100,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ControlMask,           XK_q,      quit,           {0} },
     { MODKEY|ShiftMask,             XK_d,       spawn,         SHCMD("discord-canary")},
     { MODKEY,                       XK_w,       spawn,         SHCMD("brave")},
     { 0,                            XK_Print,   spawn,         SHCMD("flameshot gui")},
